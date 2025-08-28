ace23142cd8bc064014115367ad88197468f613f
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2025-08-28 13:55:23 +0530

    Django

--- class36/project1/Inventory/views.py
+++ class36/project1/Inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Laptop, Desktop, Mobile
+#from .forms import LaptopForm, DesktopForm, MobileForm
 
 # Create your views here.
 def index(request):
